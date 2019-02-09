a516bec1848730dd4741b19c9f15172e09e2d44d
DanIzquierdo <dan.j.izquierdo@gmail.com>
2019-02-09 16:28:51 -0500

    Update car.py

--- car.py
+++ car.py
@@ -1,10 +1,39 @@
 class Car:
-
     _all = []
 
-    def __init__(self, make, model, year, owner):
+    def __init__(self, make, model, year):
         self._make = make
         self._model = model
         self._year = year
-        self._owner = owner
-        Car._all.append(self)
+        Car.all().append(self)
+    
+    @property
+    def make(self):
+        return self._make
+    @make.setter
+    def make(self, value):
+        self._make=value
+    @property
+    def model(self):
+        return self._model
+    @model.setter
+    def model(self,value):
+        self._model=value
+    @property
+    def year(self):
+        return self._year
+    @year.setter
+    def year(self,value):
+        self._year=value
+    @classmethod
+    def all(cls):
+        return cls._all
+    
+    def be_owned(self,Owner):
+        self.owner=Owner
+    @property
+    def owner(self):
+        return self._owner
+    @owner.setter
+    def owner(self,value):
+        self._owner=value
